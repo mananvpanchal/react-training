0c606797dbcb2d8976ef99abcd028adf5d5b06b8
Alex Liang <aliang@integconsulting.com>
2016-01-08 00:04:04 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,20 @@
+/*eslint-disable */
+var webpack = require('webpack');
+var WebpackDevServer = require('webpack-dev-server');
+var config = require('./webpack/webpack.config');
+
+new WebpackDevServer(webpack(config), {
+  publicPath: config.output.publicPath,
+  hot: true,
+  historyApiFallback: true,
+  contentBase:'./static',
+  stats: {
+    colors: true
+  }
+}).listen(3000, 'localhost', function (err, result) {
+  if (err) {
+    console.log(err);
+  }
+
+  console.log('Listening at localhost:3000');
+});
